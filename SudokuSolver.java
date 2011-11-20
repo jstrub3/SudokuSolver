aa677963bf46d3133314903eba89d4df067b2c50
U-Joseph-THINK\Joseph <Joseph@Joseph-THINK.(none)>
2011-11-20 14:39:50 -0600

    working parser, board set up

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -8,7 +8,10 @@
 */
 
 import java.util.*;
-import java.Strings.*;
+import java.lang.String.*;
+import java.io.*;
+import java.text.*;
+
 
 
 public class SudokuSolver {
@@ -20,22 +23,30 @@ public class SudokuSolver {
   //This is to eliminate static method errors
   public void mainloop(String[] args){
   
-  int[][] board = new array[9][9];
+  int[][] board = new int[9][9];
 
   //We will first need to get the 9x9 array, either from command line
   //or from asking for input
   
-  if(args[0] != null)
+  if(args.length!=0){
     for(int i=0; i<args.length; i++){
-      board[i/10][i%10]=args[i];
+      board[i/10][i%10]=Integer.parseInt(args[i]);
       }
-  else
-    for(int i=0; i<10; i++){
+    System.out.println("reached args!=null");
+  }
+  else 
+    for(int i=0; i<9; i++){
+      try{
       System.out.println("Please enter line number "+(i+1));
-      BufferedReader r=newBufferedReader(System.in);
-      String[] s=r.readLine().split(" ");
-      for(int j=0; j<10; j++)
-        board[i][j]=(int)s.charAt(j);
+      BufferedReader r=new BufferedReader(new 
+InputStreamReader(System.in));
+      String in=r.readLine();
+      String[] s=in.split(" ");
+      for(int j=0; j<9; j++)
+        board[i][j]=Integer.parseInt(s[j]);
+      }catch(IOException e){
+        System.out.println("Error :"+e.getMessage());
+        }
       }
 
   
@@ -44,10 +55,11 @@ public class SudokuSolver {
 
 
   //test to see that we get the correct input
-  for(int i=0; i<10; i++)
-    System.out.println(board[i].toString());
-
-    
+  for(int i=0; i<9; i++){
+    for(int j=0; j<9; j++)
+      System.out.print(board[i][j] + " ");
+    System.out.println(" ");
+    }
 
 
 
